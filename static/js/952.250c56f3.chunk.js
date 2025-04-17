6c1395c22c29b32e45c3d5ace73568e1ec39dd9a
kees <admin@walrus.cfd>
2025-04-17 19:45:45 +0100

    Updates

---
+++ static/js/952.250c56f3.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfootball_app=self.webpackChunkfootball_app||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.250c56f3.chunk.js.map
